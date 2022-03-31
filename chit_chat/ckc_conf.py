24a30e35699bbafb8ff0475243e0b72a3d92df46
gibsonbailey <gibson.bailey@live.com>
2022-03-31 16:24:08 -0700

    Make chat_settings reactive to settings changes with settings_changed signal. Add MESSAGE serializer to default settings.

--- chit_chat/ckc_conf.py
+++ chit_chat/ckc_conf.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.signals import setting_changed
 from django.utils.module_loading import import_string
 
 
@@ -17,11 +18,20 @@ chat_settings = {
     'SERIALIZERS': ImportDict({
         # NOTE: these are referenced like chat_settings.USER (as attribute, not dict key!)
         'USER': 'chit_chat.serializers.ChatUserSerializer',
+        'MESSAGE': 'chit_chat.consumer_serializers.ChatMessageSerializer',
     })
 }
 
 
-# Override default serializers..
-if hasattr(settings, 'CKC_CHAT_SERIALIZERS'):
-    for name, module in settings.CKC_CHAT_SERIALIZERS.items():
-        chat_settings['SERIALIZERS'][name] = module
+def load_or_reload_settings(setting=None, **kwargs):
+    if setting is not None and setting != 'CKC_CHAT_SERIALIZERS':
+        return
+    # Override default serializers..
+    if hasattr(settings, 'CKC_CHAT_SERIALIZERS'):
+        for name, module in settings.CKC_CHAT_SERIALIZERS.items():
+            chat_settings['SERIALIZERS'][name] = module
+
+
+load_or_reload_settings()
+
+setting_changed.connect(load_or_reload_settings)
